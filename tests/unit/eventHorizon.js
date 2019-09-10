9d0a865ac6bf10ed968b0b7cf54475e3811b7cf5
Wiktor Toporek <wiktor@tsh.io>
2019-09-10 10:22:27 +0200

    add some buggy edge case test

--- tests/unit/eventHorizon.js
+++ tests/unit/eventHorizon.js
@@ -156,3 +156,144 @@ tape('EventHorizon / Resolver test', t => {
     assertArraysMatch(data, dataResult, e => t.fail(e), () => t.pass('Relational live query'))
 })
 
+
+tape('Live query aggregated data refresh after relation update test', t => {
+  const eventHorizon = new EventHorizon()
+
+  const people = eventHorizon.createTesseract('person', {
+    id: 'person',
+    columns: [{
+      name: 'id',
+      primaryKey: true,
+    }, {
+      name: 'name',
+    }]
+  })
+
+  const messages = eventHorizon.createTesseract('message', {
+    id: 'message',
+    columns: [{
+      name: 'id',
+      primaryKey: true
+    }, {
+      name: 'personId',
+    }, {
+      name: 'status'
+    }]
+  })
+
+  const session = eventHorizon.createSession({
+    table: 'person',
+    subSessions: {
+      messageAggregator: {
+        table: 'message',
+        columns: [
+          { name: 'personId', primaryKey: true },
+          { name: 'status' },
+          { name: 'count', value: 1, aggregator: 'sum' },
+        ],
+        groupBy: [{ dataIndex: 'personId' }],
+        filter: [{ field: 'status', comparison: 'in', value: ['sent', 'delivered'] }]
+      }
+    },
+    filter: [],
+    columns: [
+      { name: 'id', primaryKey: true },
+      {
+        name: 'numberOfMessages',
+        resolve: {
+          underlyingField: 'id',
+          session: 'messageAggregator',
+          childrenTable: 'message',
+          displayField: 'count'
+        }
+      }
+    ]
+  })
+
+  /****************
+   * EXPECTATIONS *
+   ****************/
+  const expectedResult = [
+    // After adding persons
+    {
+      addedIds: [ 1, 2, 3 ],
+      addedData: [
+        { id: 1, numberOfMessages: undefined },
+        { id: 2, numberOfMessages: undefined },
+        { id: 3, numberOfMessages: undefined }
+      ],
+      updatedIds: [],
+      updatedData: [],
+      removedIds: [],
+      removedData: []
+    },
+
+    // After adding messages
+    {
+      addedIds: [],
+      addedData: [],
+      updatedIds: [ 1, 2, 3 ],
+      updatedData: [
+        { id: 1, numberOfMessages: 3 },
+        { id: 2, numberOfMessages: 2 },
+        { id: 3, numberOfMessages: 1 }
+      ],
+      removedIds: [],
+      removedData: []
+    },
+
+    // After changing one of the messages status
+    {
+      addedIds: [],
+      addedData: [],
+      updatedIds: [ 3 ],
+      updatedData: [
+        { id: 3, numberOfMessages: undefined }
+      ],
+      removedIds: [],
+      removedData: []
+    }
+  ]
+
+  const updates$ = fromEvent(session, 'dataUpdate')
+    .pipe(map(([d]) => d))
+
+  tapeAssert(t, updates$, expectedResult.map(flat), 'Updates match', 300)
+    .toPromise()
+    .then(() => t.end())
+
+  /************
+   * SCENARIO *
+   ************/
+  people.add([
+    { id: 1, name: 'Person #1' },
+    { id: 2, name: 'Person #2' },
+    { id: 3, name: 'Person #3' },
+  ])
+
+  messages.add([
+    // Person #1 visible messages count 3
+    { id: 1, personId: 1, status: 'delivered' },
+    { id: 2, personId: 1, status: 'delivered' },
+    { id: 3, personId: 1, status: 'sent' },
+    { id: 4, personId: 1, status: 'deleted' },
+
+    // Person #2 visible messages count 2
+    { id: 5, personId: 2, status: 'delivered' },
+    { id: 6, personId: 2, status: 'deleted' },
+    { id: 7, personId: 2, status: 'sent' },
+
+    // Person #1 visible messages count 1
+    { id: 8, personId: 3, status: 'sent' },
+  ])
+
+  // Make person #3 visible messages count 0
+  setTimeout(() => {
+    messages.update([
+      { id: 8, status: 'deleted' },
+    ])
+  }, 100)
+
+
+})
